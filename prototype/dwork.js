6ca7a63c90266ab9c8f3654f789f8acb0bab036d
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-05-03 17:04:05 -0700

    added binding to zipcode enter button.  No form element to grab yet.

--- prototype/dwork.js
+++ prototype/dwork.js
@@ -1,7 +1,24 @@
 $(document).ready (function(){
-	var options = {};
-	$('.modal').modal(options);
+  var options = {};
+  $('.modal').modal(options);
+
+  var binder = new dworkApp.Binder();
+  binder.bind();
+
 });
 
+function dworkApp(){};
 
+dworkApp.Binder = function() {};
 
+dworkApp.Binder.prototype = {
+  bind: function() {
+    this.bindEnterZip();
+  },
+
+  bindEnterZip: function() {
+    $('.btn.btn-primary').on("click", function() {
+      console.log("enter button clicked");
+    });
+  }
+};
