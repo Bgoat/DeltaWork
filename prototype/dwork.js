0517d85cf4903b9cc6b151da5a21b40a612ebbdf
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-05-03 19:16:17 -0700

    zipcode entry and lookup now functioning.  need to merge in most recent styling changes.

--- prototype/dwork.js
+++ prototype/dwork.js
@@ -20,9 +20,17 @@ dworkApp.Binder.prototype = {
     $('#zip-submit').on("click", function(e) {
       e.preventDefault();
       console.log("enter button clicked");
-      //do ajax call here, get tariff back
-      //once it returns:
-      $('button.close').click()
+      var userZip = $('#zipcode').val();
+      $.getJSON( "http://api.genability.com/rest/prices?appId=894a0759-f682-4760-b160-ecefba051e7b&appKey=ad02f158-8b1d-4cac-8fda-8d21c8248752&zipCode=" + userZip + "&customerClasses=RESIDENTIAL&tariffTypes=DEFAULT", function(data){
+        var tariffs = _.each(data.results, function(tariff) {
+          tariff["roundedRate"] = tariff.rateMean.toFixed(2);
+        })
+        var template= $('#tariffInfo').html();
+        var output = Mustache.render(template, {tariffs: tariffs});
+        $('#tariffInHere').html(output);
+        $('button.close').click()
+      })
     });
   }
-};
+}
+
