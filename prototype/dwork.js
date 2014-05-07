21f2b689066d09a101c2b3bc1725482b17f4f0ec
Matt Bechtel <matt.b.bechtel@gmail.com>
2014-05-06 17:44:24 -0700

    swapping out my keys for Bill's from Genability api

--- prototype/dwork.js
+++ prototype/dwork.js
@@ -25,7 +25,7 @@ dworkApp.Controller.prototype = {
   },
 
   getTariffOnZip: function(userZip) {
-    $.getJSON("http://api.genability.com/rest/prices?appId=894a0759-f682-4760-b160-ecefba051e7b&appKey=ad02f158-8b1d-4cac-8fda-8d21c8248752&zipCode=" + userZip + "&customerClasses=RESIDENTIAL&tariffTypes=DEFAULT", function(data){
+    $.getJSON("http://api.genability.com/rest/prices?appId=cbd5a381-d181-43c2-87d9-78eba2ee20e8&appKey=e6cf596c-f151-42a2-b397-e83e85861239&zipCode=" + userZip + "&customerClasses=RESIDENTIAL&tariffTypes=DEFAULT", function(data){
       var outputTariff = data.results[0].rateMean.toFixed(2) *100;
       $('#tariffInHere').html(outputTariff + "&cent;");
       var outputRenew = (data.results[0].rateMean * 108).toFixed(0);
@@ -35,7 +35,7 @@ dworkApp.Controller.prototype = {
   },
 
   getNameOnZip: function(userZip) {
-    $.getJSON("http://api.genability.com/rest/public/lses?appId=894a0759-f682-4760-b160-ecefba051e7b&appKey=ad02f158-8b1d-4cac-8fda-8d21c8248752&zipCode=" + userZip + "&customerClasses=RESIDENTIAL&tariffTypes=DEFAULT", function(data){
+    $.getJSON("http://api.genability.com/rest/public/lses?appId=cbd5a381-d181-43c2-87d9-78eba2ee20e8&appKey=e6cf596c-f151-42a2-b397-e83e85861239&zipCode=" + userZip + "&customerClasses=RESIDENTIAL&tariffTypes=DEFAULT", function(data){
       var outputName = data.results[0].name;
       $('#nameInHere').html("<div class='util-name'>" + outputName + "</div>");
       var outputRenewName = "<div class='util-name'>Oasis Energy</div>";
